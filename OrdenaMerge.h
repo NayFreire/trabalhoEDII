77f496e68de2cc44e0fc1b36b894bc8a1de1de3b
Nayara <nayfreorepf@gmail.com>
2021-10-23 12:52:40 -0300

    Criada a lista duplamente encadeada

---
+++ OrdenaMerge.h
@@ -0,0 +1,63 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int counter = 0;
+void numComparacoes(){
+    counter++;
+}
+
+void merge(int vetor[], int inicio, int meio, int fim){
+    /* for(int i=0;i<n;i++){
+        printf("%d\n", vetor[i]);
+    }
+    printf("\n----------------------------------------\n"); */
+
+    int tamanho = fim - inicio + 1;
+    int fim1 = 0, fim2 = 0;
+    int p1 = inicio, p2 = meio + 1;
+
+    int *temp = (int*) malloc(tamanho * sizeof(int));
+
+    for(int i=0;i<=tamanho-1;i++){
+        if(!fim1 && !fim2){
+        if(vetor[p1] < vetor[p2]){
+            numComparacoes();
+            temp[i] = vetor[p1];
+            p1++;
+        }
+        else{
+            temp[i] = vetor[p2];
+            p2++;
+        }
+
+        if(p1 > meio){
+            fim1 = 1;
+        }
+        if(p2 > fim){
+            fim2 = 1;
+        }
+        }
+        else{
+        if(!fim1){
+            temp[i] = vetor[p1];
+            p1++;
+        }
+        else{
+            temp[i] = vetor[p2];
+            p2++;
+        }
+        }
+    }
+    for(int j=0, k=inicio;j<=tamanho-1;j++,k++){
+        vetor[k] = temp[j];
+    }
+}
+
+int mergeSort(int vetor[], int inicio, int fim){
+    if(inicio < fim){
+        int meio = (inicio+fim)/2;
+        mergeSort(vetor, inicio, meio);
+        mergeSort(vetor, meio + 1, fim);
+        merge(vetor, inicio, meio, fim);
+    }
+}
--- edIItrabalho.c
+++ edIItrabalho.c
@@ -1,70 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define n 10000
+#define n 100
 
-int counter = 0;
-
-void numComparacoes(){
-    counter++;
-}
-
-void merge(int vetor[], int inicio, int meio, int fim){
-    /* for(int i=0;i<n;i++){
-        printf("%d\n", vetor[i]);
-    }
-    printf("\n----------------------------------------\n"); */
-
-    int tamanho = fim - inicio + 1;
-    int fim1 = 0, fim2 = 0;
-    int p1 = inicio, p2 = meio + 1;
-
-    int *temp = (int*) malloc(tamanho * sizeof(int));
-
-    for(int i=0;i<=tamanho-1;i++){
-        if(!fim1 && !fim2){
-        if(vetor[p1] < vetor[p2]){
-            numComparacoes();
-            temp[i] = vetor[p1];
-            p1++;
-        }
-        else{
-            temp[i] = vetor[p2];
-            p2++;
-        }
-
-        if(p1 > meio){
-            fim1 = 1;
-        }
-        if(p2 > fim){
-            fim2 = 1;
-        }
-        }
-        else{
-        if(!fim1){
-            temp[i] = vetor[p1];
-            p1++;
-        }
-        else{
-            temp[i] = vetor[p2];
-            p2++;
-        }
-        }
-    }
-    for(int j=0, k=inicio;j<=tamanho-1;j++,k++){
-        vetor[k] = temp[j];
-        printf("%d - ", temp[j]);
-    }
-}
-
-int mergeSort(int vetor[], int inicio, int fim){
-    if(inicio < fim){
-        int meio = (inicio+fim)/2;
-        mergeSort(vetor, inicio, meio);
-        mergeSort(vetor, meio + 1, fim);
-        merge(vetor, inicio, meio, fim);
-    }
-}
+#include "ListaEncadeada.h"
+#include "OrdenaMerge.h"
 
 void main(){
     // int n = 10;
@@ -75,7 +15,7 @@ void main(){
     srand(time(NULL));
 
     for(int i=0;i<n;i++){
-        vetor[i] = rand()%100000;
+        vetor[i] = rand()%1000;
     }
     printf("\nINICIO DO VETOR DESORDENADO\n");
     for(int i=0;i<n;i++){
@@ -90,4 +30,10 @@ void main(){
     }
 
     printf("Comparacoes: %d\n", counter);
+
+    criaLista();
+    for(int i=0;i<10;i++){
+        insereInicio(rand()%1000);
+    }
+    imprimeLista();
 }
