699563332c270681f1942294f2922769931b08ee
Rafael Vinícius de Oliveira <contato.rvproductions@gmail.com>
2021-10-24 15:35:50 -0300

    Update shellSort.cpp

--- shellSort.cpp
+++ shellSort.cpp
@@ -7,89 +7,117 @@
 #include <limits.h>
 #include <sys/time.h>
 
-#define n 100000
-#define _INT_MAX +2147483647
-#define qtdbuscas 10000000
-#define ms 1000000 
+#define n 500000
 #define _BSD_SOURCE
 
 //Struct Tempo
 struct timeval comeco, final;
 
-//Numero de Comparações
-int counter = 0;
+//Struct Elemento
+typedef struct elemento Elemento;
+struct elemento{
+	int valor;
+};
+
+//Função para o Numero de Comparações
+int counter=0;
 void numComparacoes(){
     counter++;
 }
 
-//Gerar Codigo Aleatorio
-void CodigoAleatorio(int *vet){
+//Função para Gerar Numero Aleatorio
+void NumeroAleatorio(elemento *vetor){
 	int i=0;
 	
 	srand((unsigned)time(NULL));
 	
 	for(i=0; i<n; i++){
-		vet[i] = 1 + rand() % 100000000000000 * rand() % 100000000000000 + rand() % 100000000000000 - rand() % 100000000 * rand() % n;
+		vetor[i].valor = 1 + rand() % 100000000 * rand() % 100000000 + rand() % 100000000 - rand() % 100000000 * rand() % n;
 	}
-	
-	//vet[i] = 1 + rand() % 100000000000000 * rand() % 100000000000000 + rand() % 100000000000000 - rand() % 100000000 * rand() % n;
 }
 
-
-//Shell Sort
-void shellSort(int *vet, int tam) {
+//Função Shell Sort
+void shellSort(elemento *vetor, int tam){
     int i, j, valor;
  
     int h = 1;
-    while(h < tam) {
-        h = 3*h+1;
+    while(h < tam){
+        h = 3 * h + 1;
     }
-    while (h > 0) {
-        for(i = h; i < tam; i++) {
-            valor = vet[i];
+    while(h > 0){
+        for(i = h; i < tam; i++){
+            valor = vetor[i].valor;
             j = i;
-            while (j > h-1 && valor <= vet[j - h]) {
+            while(j > h-1 && valor <= vetor[j - h].valor){
             	numComparacoes();
-                vet[j] = vet[j - h];
+                vetor[j].valor = vetor[j - h].valor;
                 j = j - h;
             }
-            vet[j] = valor;
+            vetor[j].valor = valor;
         }
         h = h/3;
     }
 }
 
+//Main
 int main(){
-	int vet[n];
+	
+	FILE *fileList;
+	char result;
+	
 	int i;
+	elemento vetor[n];
 	
 	srand(time(NULL));
 	
-	CodigoAleatorio(vet);
+	NumeroAleatorio(vetor);
 	
-	printf("Vetor Inicial Desordenado\n");
+	printf("Vetor Inicial Desordenado");
 	
+	/*
 	//Apresentar vetor desordenado
 	for(i=0; i<n; i++){
-		printf("\t%d", vet[i]);
+		printf("%d\n", vetor[i].valor);
 	}
+	*/
 	
-	printf("\n\n-----------------------------------------------\n\n");
-	
-	//Chamada da função
+	//Chamada da função Shell e funcoes de tempo
 	gettimeofday(&comeco, NULL);
-	shellSort(vet, n);
+	shellSort(vetor, n);
 	gettimeofday(&final, NULL);
 	
-	printf("Vetor Final Ordenado\n");
+	printf("\n-----------------------------------------------\n");
+	printf("Vetor Final Ordenado");
 	
+	printf("\n-----------------------------------------------\n");
+	printf("Resultado exportado no arquivo");
+	
+	/*
 	//Apresentar vetor ordenado
 	for(i=0; i<n; i++){
-		printf("\t%d", vet[i]);
+		printf("%d\n", vetor[i].valor);
 	}
+	*/
 	
-	printf("\n\n-----------------------------------------------\n\n");
-	
+	/*
+	//Printf com numero de comparacoes e tempo
 	printf("Numero de Comparacoes: %d", counter);
 	printf("\n\nTempo de processamento: %ld microsegundos\n\n", ((final.tv_sec - comeco.tv_sec)*1000000L+final.tv_usec) - comeco.tv_usec);
+	*/
+	
+	//fclose(fileList);
+    fileList = fopen("ShellSort.txt", "wt");
+    if(fileList != NULL){
+        if(result == EOF){
+            printf("Erro ao gravar no arquivo");
+    	}
+    }
+	else{
+        printf("Erro ao abrir/criar o arquivo");
+    }
+    
+    //Escrever no arquivo os resultados
+    result = fprintf(fileList, "  ShellSort		N: %d\n", n);
+    result = fprintf(fileList, "\n  Microsegundos:	%ld", ((final.tv_sec - comeco.tv_sec)*1000000L+final.tv_usec) - comeco.tv_usec);
+	result = fprintf(fileList, "\n  Comparações:		%d", counter); 
 }
