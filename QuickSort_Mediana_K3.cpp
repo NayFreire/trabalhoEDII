507d104ae1dbbad4fc08fa697df0e96f80ac1e9e
Rafael Vinícius de Oliveira <contato.rvproductions@gmail.com>
2021-10-25 19:39:41 -0300

    Create QuickSort_Mediana_K3.cpp

---
+++ QuickSort_Mediana_K3.cpp
@@ -0,0 +1,172 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <conio.h>
+#include <string.h>
+#include <windows.h>
+#include <limits.h>
+#include <sys/time.h>
+
+#define n 500000
+#define _BSD_SOURCE
+
+//Struct Tempo
+struct timeval comeco, final;
+
+//Struct Elemento
+typedef struct elemento Elemento;
+struct elemento{
+	int valor;
+};
+
+//Função para o Numero de Comparações
+int counter=0;
+void numComparacoes(){
+    counter++;
+}
+
+//Função que realiza a troca entre dois elementos
+void troca(elemento *vetor, int i, int j){
+	int aux = vetor[i].valor;
+	vetor[i].valor = vetor[j].valor;
+	vetor[j].valor = aux;
+}
+
+//Função que pega os 3 indices aleatorios e encontra a mediana
+int Aleatorio(elemento *vetorAux, int inicio, int meio, int fim){
+	
+	int i, auxV, pivoMediana;
+	
+	while(meio < inicio || meio > fim){
+		if(inicio > meio){
+			auxV = meio;
+			meio = inicio;
+			inicio = auxV;
+		}
+		if(meio > fim){
+			auxV = fim;
+			fim = meio;
+			meio = auxV;
+		}
+	}	
+	
+	vetorAux[0].valor = inicio;
+	vetorAux[1].valor = meio;
+	vetorAux[2].valor = fim;
+
+	pivoMediana = meio;
+	
+	return pivoMediana;
+}
+
+//Particiona e retorna o índice do pivô
+int Particiona(elemento *vetor, int inicio, int fim){
+	int pivo, pivo_indice, i;
+	
+	pivo = vetor[fim].valor;
+	pivo_indice = inicio;
+	
+	for(i = inicio; i < fim; i++){
+		if(vetor[i].valor <= pivo){
+			numComparacoes();
+			
+			troca(vetor, i, pivo_indice);
+
+			pivo_indice++;
+		}
+	}
+	
+	troca(vetor, pivo_indice, fim);
+	
+	return pivo_indice;
+}
+
+int ParticionaRandom(elemento *vetor, int inicio, int fim){
+	int i;
+	
+	elemento vetorAux[3];
+	
+	int pivo_indice1 = (rand() % (fim - inicio + 1)) + inicio;
+	int pivo_indice2 = (rand() % (fim - inicio + 1)) + inicio;
+	int pivo_indice3 = (rand() % (fim - inicio + 1)) + inicio;
+	
+	int pivo_indice = (Aleatorio(vetorAux, pivo_indice1, pivo_indice2, pivo_indice3));
+	
+	troca(vetor, pivo_indice, fim);
+
+	return Particiona(vetor, inicio, fim);
+}
+
+void QuickSort(elemento *vetor, int inicio, int fim){
+	if(inicio < fim){
+		//Função particionar retorna o índice do pivô
+		int pivo_indice = ParticionaRandom(vetor, inicio, fim);
+
+		QuickSort(vetor, inicio, pivo_indice - 1);
+		QuickSort(vetor, pivo_indice + 1, fim);
+	}
+}
+
+void NumeroAleatorio(elemento *vetor){
+	int i=0;
+	
+	srand((unsigned)time(NULL));
+	
+	for(i=0; i<n; i++){
+		vetor[i].valor = 1 + rand() % 100000000 * rand() % 100000000 + rand() % 100000000 - rand() % 100000000 * rand() % n;
+	}
+}
+
+int main(){
+	
+	FILE *fileList;
+	char result;
+	
+	elemento vetor[n];
+	NumeroAleatorio(vetor);
+	
+	int tam_vet = sizeof(vetor) / sizeof(int);
+	int i;
+	
+	srand(time(NULL));
+	
+	printf("Vetor Inicial Desordenado");
+	
+	/*for(i=0; i<n; i++){
+		printf("%d\n", vetor[i].valor);
+	}*/
+	
+	gettimeofday(&comeco, NULL);
+	QuickSort(vetor, 0, tam_vet-1);
+	gettimeofday(&final, NULL);
+	
+	printf("\n-----------------------------------------------\n");
+	printf("Vetor Final Ordenado");
+	
+	/*for(i=0; i<n; i++){
+		printf("%d\n", vetor[i].valor);
+	}*/
+	
+	printf("\n-----------------------------------------------\n");
+	printf("Resultado exportado no arquivo");
+	
+	//fclose(fileList);
+    fileList = fopen("QuickSort_Mediana_K3.txt", "wt");
+    if(fileList != NULL){
+        if(result == EOF){
+            printf("Erro ao gravar no arquivo");
+    	}
+    }
+	else{
+        printf("Erro ao abrir/criar o arquivo");
+    }
+    
+    //Escrever no arquivo os resultados
+    result = fprintf(fileList, "  QuickSort_Mediana_K3		N: %d\n", n);
+    result = fprintf(fileList, "\n  Microsegundos:	%ld", ((final.tv_sec - comeco.tv_sec)*1000000L+final.tv_usec) - comeco.tv_usec);
+	result = fprintf(fileList, "\n  Comparações:		%d", counter); 
+	
+	printf("\n\nNumero de Comparacoes: %d", counter);
+	printf("\n\nTempo de processamento: %ld microsegundos\n\n", ((final.tv_sec - comeco.tv_sec)*1000000L+final.tv_usec) - comeco.tv_usec);
+	
+}
